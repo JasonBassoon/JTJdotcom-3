90e05cd8437554b167a7dd19617023a15834a818
Jason Ettinger <spamforacautiousman@gmail.com>
2026-03-23 11:04:52 -0500

    Remove 'View Case Study' button

--- src/App.tsx
+++ src/App.tsx
@@ -59,14 +59,6 @@ function App() {
             <div id="hero">
               <Hero />
             </div>
-            <div className="container" style={{ paddingTop: '2rem', paddingBottom: '2rem', textAlign: 'center' }}>
-              <button
-                onClick={() => setShowCaseStudy(true)}
-                className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors text-lg font-semibold"
-              >
-                View Case Study
-              </button>
-            </div>
             <WhatsNew />
             <Projects onShowCaseStudy={() => setShowCaseStudy(true)} />
             <Learning />
